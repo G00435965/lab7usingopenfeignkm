1af714961633c0ced5d880b1b250da4a31eaa8cf
KEELANMURPHY-STUDENT <G00435965@atu.ie>
2025-12-03 15:19:06 +0000

    new project

---
+++ src/main/java/ie/atu/lab7/lab7usingopenfeign/Lab7usingopenfeignApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab7.lab7usingopenfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7usingopenfeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab7usingopenfeignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab7/lab7usingopenfeign/Lab7usingopenfeignApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab7.lab7usingopenfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7usingopenfeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
